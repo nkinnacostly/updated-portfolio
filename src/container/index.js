8d1f0e9aed318e6deb4c0381ee730bd45b203255
Costly Ayang <68037498+nugi-developers@users.noreply.github.com>
2022-02-19 10:13:05 +0100

    Done with header

---
+++ src/container/index.js
@@ -0,0 +1,6 @@
+export { default as About } from "./about/about"
+export { default as Footer } from "./footer/footer"
+export { default as Header } from "./header/header"
+export { default as Skills } from "./skills/skills"
+export { default as Testimonials } from "./testimonials/testimonial"
+export { default as Work } from "./work/work"
---
+++ src/container/work/work.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./work.scss";
+
+const Work = () => {
+  return <div>Work</div>;
+};
+
+export default Work;
