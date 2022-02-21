a0b1ac680b5fa19e930c10c16cd4e07b927cd535
Costly Ayang <68037498+nugi-developers@users.noreply.github.com>
2022-02-21 23:14:33 +0100

    Fixed errors

--- src/container/skills/skills.jsx
+++ src/container/skills/skills.jsx
@@ -25,12 +25,12 @@ const Skills = () => {
       <h2 className="head-text">Skills & Experiences</h2>
       <div className="app__skills-container">
         <motion.div className="app__skills-list">
-          {skills?.map((skill) => (
+          {skills?.map((skill, i) => (
             <motion.div
               whileInView={{ opacity: [0, 1] }}
               transition={{ duration: 0.5 }}
               className="app__skills-item app__flex"
-              key={skill.name}
+              key={skill.name + i}
             >
               <div
                 className="app__flex"
@@ -44,34 +44,37 @@ const Skills = () => {
         </motion.div>
 
         <motion.div className="app__skills-exp-works">
-          {experiences?.map((experience) => (
-            <motion.div className="app__skills-exp-item" key={experience.year}>
+          {experiences?.map((experience, i) => (
+            <motion.div
+              className="app__skills-exp-item"
+              key={experience.year + i}
+            >
               <div className="app__skills-exp-year">
                 <p className="bold-text">{experience.year}</p>
               </div>
               <motion.div className="app__skills-exp-works">
-                {experience?.works?.map((work) => (
-                  <>
+                {experience?.works?.map((work, i) => (
+                  <div key={i}>
                     <motion.div
                       whileInView={{ opacity: [0, 1] }}
                       transition={{ duration: 0.5 }}
                       className="app__skills-exp-work"
                       data-tip
                       data-for={work.name}
-                      key={work.name}
+                      key={work.name + i}
                     >
                       <h4 className="bold-text">{work.name}</h4>
                       <p className="p-text">{work.company}</p>
                     </motion.div>
                     <ReactTooltip
-                      id={work.name}
+                      id={work.name + i}
                       effect="solid"
                       arrowColor="#fff"
                       className="skills-tooltip"
                     >
                       {work.desc}
                     </ReactTooltip>
-                  </>
+                  </div>
                 ))}
               </motion.div>
             </motion.div>
